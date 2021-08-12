fd98a29f5d801e3f9b5c7c9745884eb8087fe60f
Shubham8968 <Shivamno1420@gmail.com>
2021-08-12 16:40:33 +0530

    this is my Dispatch code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
 
+//Store will the store is a place to store dataas 
+let store = createstore(counter)
+//action
+const increment = () =>{
+  return{
+    type: "DECREMENT"
+  }
+}
+//reducer will tell us what is uspposed happen when we perform an action 
+const counter =(state = 0,action) => {
+  switch (action.type){
+    case"INCREMENT":
+      return state+1
+      case "DECREMENT":
+        return state-1 
+  }
+}
+//store 
+let store= createStore(counter)
+//to display our counter in the console window 
+store.subscribe(() => console.log(store.getState())); // we are loggin the current state of our nstore  
+//Dispatcher
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +38,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
